9ce72fa1d2f8afb01550665db548e7dc77f0533c
GreatCheda <ogunlajasamuelmacheda@gmail.com>
2022-09-08 15:57:10 -0700

    daddy yo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
+#include<stdio.h>
 /**
-main - program execution starts here
-Description: this is the main function
-Return: an integer to signify success or fail
+*main - print size of data
+*
+*Return: Always 0
 */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					59, 1, stderr);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
