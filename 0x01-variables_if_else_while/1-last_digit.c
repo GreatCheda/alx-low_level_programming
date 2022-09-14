ec14fb58cc2e91b1cc38d8bdb49da1ea5b28c37d
GreatCheda <ogunlajasamuelmacheda@gmail.com>
2022-09-13 17:05:47 -0700

    toto

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	printf("Last digit of %i is %i and is ", n, (n % 10));
+
+	if ((n % 10) == 0)
 	{
-		printf("%i is positive\n", n);
+		printf("0\n");
 	}
-	else if (n == 0)
+	else
 	{
-		printf("%i is zero\n", n);
+		printf("less than 6 and not 0\n");
 	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}	
 
 	return (0);
-}
+}	
